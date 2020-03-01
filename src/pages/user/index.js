5447ba919bfc9833cc357be96f86a35830052ded
Goran Mekić <meka@tilda.center>
2020-03-02 00:29:48 +0100

    Use profile as user detail

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,8 +1,13 @@
-import detail from './detail'
+import detail from '../profile/detail'
 import list from './list'
+import initial from './initial'
+import { User } from 'freenit'
 
 
 export default {
   detail,
+  initial,
   list,
+  service: User.service,
+  store: User.store,
 }
